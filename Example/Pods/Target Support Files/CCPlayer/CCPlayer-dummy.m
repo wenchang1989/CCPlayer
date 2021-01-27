33ae79ecc223d045b36050d65ac822ee89f4f8fd
文昌  陈 <chenwenchang@infinities.com.cn>
2021-01-27 16:53:53 +0800

    提交

---
+++ Example/Pods/Target Support Files/CCPlayer/CCPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CCPlayer : NSObject
+@end
+@implementation PodsDummy_CCPlayer
+@end
---
+++ Example/Pods/Target Support Files/Pods-CCPlayer_Example/Pods-CCPlayer_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CCPlayer_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CCPlayer_Example
+@end
